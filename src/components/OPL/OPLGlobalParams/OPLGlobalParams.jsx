79c56a747dff1938812809f67afdd41bd0e64667
Dan Fry <57286600+danielrfry@users.noreply.github.com>
2025-12-26 12:40:24 +0000

    Convert OPLGlobalParams to function component.

--- src/components/OPL/OPLGlobalParams/OPLGlobalParams.jsx
+++ src/components/OPL/OPLGlobalParams/OPLGlobalParams.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ColumnsLayout from '../../ColumnsLayout/ColumnsLayout';
 import TitledGroup from '../../TitledGroup/TitledGroup';
 import ConnectionButton from '../../ConnectionButton/ConnectionButton';
@@ -13,95 +12,73 @@ import Button from '../../Button/Button';
 import './OPLGlobalParams.css';
 import UIGroupEnabledContext from '../../UIGroupEnabledContext/UIGroupEnabledContext';
 
-class OPLGlobalParams extends React.Component {
-    getConnectionButtons = () => {
-        const { is4Op } = this.props;
+const getConnectionButtons = (is4Op) => {
+    if (is4Op) {
+        return (
+            <>
+                <ConnectionButton path={`${Modes.OPL}.conn`} set={0} is4Op />
+                <ConnectionButton path={`${Modes.OPL}.conn`} set={2} is4Op />
+                <ConnectionButton path={`${Modes.OPL}.conn`} set={1} is4Op />
+                <ConnectionButton path={`${Modes.OPL}.conn`} set={3} is4Op />
+            </>
+        );
+    } else {
+        return (
+            <>
+                <ConnectionButton path={`${Modes.OPL}.conn`} set={0} />
+                <ConnectionButton path={`${Modes.OPL}.conn`} set={1} />
+            </>
+        );
+    }
+};
 
-        if (is4Op) {
-            return (
-                <>
-                    <ConnectionButton
-                        path={`${Modes.OPL}.conn`}
-                        set={0}
-                        is4Op
-                    />
-                    <ConnectionButton
-                        path={`${Modes.OPL}.conn`}
-                        set={2}
-                        is4Op
-                    />
-                    <ConnectionButton
-                        path={`${Modes.OPL}.conn`}
-                        set={1}
-                        is4Op
-                    />
-                    <ConnectionButton
-                        path={`${Modes.OPL}.conn`}
-                        set={3}
-                        is4Op
-                    />
-                </>
-            );
-        } else {
-            return (
-                <>
-                    <ConnectionButton path={`${Modes.OPL}.conn`} set={0} />
-                    <ConnectionButton path={`${Modes.OPL}.conn`} set={1} />
-                </>
-            );
-        }
+const OPLGlobalParams = ({ enabled, is4Op, onSet4Op }) => {
+    const _handle2OpClicked = () => {
+        onSet4Op(false);
     };
 
-    render = () => {
-        const { enabled, is4Op } = this.props;
+    const _handle4OpClicked = () => {
+        onSet4Op(true);
+    };
 
-        return (
-            <UIGroupEnabledContext.Provider value={enabled}>
-                <div
-                    className={classNames('opl-patch-params', {
-                        'ui-element--enabled': enabled,
-                        'ui-element--disabled': !enabled,
-                    })}
-                >
-                    <ColumnTitle>GLOBAL</ColumnTitle>
-                    <div>
-                        <TitledGroup title="CONNECTION">
-                            <ColumnsLayout
-                                stretchH
-                                extraClasses="opl-patch-params__ops-enabled-row"
+    return (
+        <UIGroupEnabledContext.Provider value={enabled}>
+            <div
+                className={classNames('opl-patch-params', {
+                    'ui-element--enabled': enabled,
+                    'ui-element--disabled': !enabled,
+                })}
+            >
+                <ColumnTitle>GLOBAL</ColumnTitle>
+                <div>
+                    <TitledGroup title="CONNECTION">
+                        <ColumnsLayout
+                            stretchH
+                            extraClasses="opl-patch-params__ops-enabled-row"
+                        >
+                            <Button
+                                highlighted={!is4Op}
+                                onClick={_handle2OpClicked}
                             >
-                                <Button
-                                    highlighted={!is4Op}
-                                    onClick={this._handle2OpClicked}
-                                >
-                                    2-OP
-                                </Button>
-                                <Button
-                                    highlighted={is4Op}
-                                    onClick={this._handle4OpClicked}
-                                >
-                                    4-OP
-                                </Button>
-                            </ColumnsLayout>
-                            {this.getConnectionButtons()}
-                        </TitledGroup>
-                    </div>
-                    <TitledGroup title="FEEDBACK" withMargin={false}>
-                        <ParamKnob path={`${Modes.OPL}.fb`} />
+                                2-OP
+                            </Button>
+                            <Button
+                                highlighted={is4Op}
+                                onClick={_handle4OpClicked}
+                            >
+                                4-OP
+                            </Button>
+                        </ColumnsLayout>
+                        {getConnectionButtons(is4Op)}
                     </TitledGroup>
                 </div>
-            </UIGroupEnabledContext.Provider>
-        );
-    };
-
-    _handle2OpClicked = () => {
-        this.props.onSet4Op(false);
-    };
-
-    _handle4OpClicked = () => {
-        this.props.onSet4Op(true);
-    };
-}
+                <TitledGroup title="FEEDBACK" withMargin={false}>
+                    <ParamKnob path={`${Modes.OPL}.fb`} />
+                </TitledGroup>
+            </div>
+        </UIGroupEnabledContext.Provider>
+    );
+};
 
 const mapStateToProps = (state) => ({
     is4Op: state.patchEditor.patch[Modes.OPL].is4Op,
