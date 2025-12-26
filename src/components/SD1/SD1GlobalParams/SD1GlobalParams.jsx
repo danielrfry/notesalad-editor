4bf35f316b78171fe83cb55038418ab3d872f379
Dan Fry <57286600+danielrfry@users.noreply.github.com>
2025-12-26 12:29:57 +0000

    Convert SD1GlobalParams to function component.

--- src/components/SD1/SD1GlobalParams/SD1GlobalParams.jsx
+++ src/components/SD1/SD1GlobalParams/SD1GlobalParams.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ColumnTitle from '../../ColumnTitle/ColumnTitle';
 import TitledGroup from '../../TitledGroup/TitledGroup';
 import classNames from 'classnames';
@@ -12,70 +11,68 @@ import UIGroupEnabledContext from '../../UIGroupEnabledContext/UIGroupEnabledCon
 
 import './SD1GlobalParams.css';
 
-export default class SD1GlobalParams extends React.Component {
-    render = () => {
-        const { enabled, patch } = this.props;
-        const is4Op = isSD1Patch4Op(patch);
+const getConnectionButtons = (is4Op) =>
+    is4Op ? (
+        <>
+            <ConnectionButton path={`${Modes.SD1}.alg`} set={2} />
+            <ConnectionButton path={`${Modes.SD1}.alg`} set={3} />
+            <ConnectionButton path={`${Modes.SD1}.alg`} set={4} />
+            <ConnectionButton path={`${Modes.SD1}.alg`} set={5} />
+            <ConnectionButton path={`${Modes.SD1}.alg`} set={6} />
+            <ConnectionButton path={`${Modes.SD1}.alg`} set={7} />
+        </>
+    ) : (
+        <>
+            <ConnectionButton path={`${Modes.SD1}.alg`} set={0} />
+            <ConnectionButton path={`${Modes.SD1}.alg`} set={1} />
+        </>
+    );
 
-        return (
-            <UIGroupEnabledContext.Provider value={enabled}>
-                <div
-                    className={classNames('sd1-patch-params', {
-                        'ui-element--enabled': enabled,
-                        'ui-element--disabled': !enabled,
-                    })}
-                >
-                    <ColumnTitle>GLOBAL</ColumnTitle>
-                    <TitledGroup title="CONNECTION">
-                        <ColumnsLayout
-                            stretchH
-                            extraClasses="sd1-patch-params__ops-enabled-row"
+const SD1GlobalParams = ({ enabled, patch, onSet4Op }) => {
+    const is4Op = isSD1Patch4Op(patch);
+    const _handleSet2OpClicked = () => onSet4Op(false);
+    const _handleSet4OpClicked = () => onSet4Op(true);
+
+    return (
+        <UIGroupEnabledContext.Provider value={enabled}>
+            <div
+                className={classNames('sd1-patch-params', {
+                    'ui-element--enabled': enabled,
+                    'ui-element--disabled': !enabled,
+                })}
+            >
+                <ColumnTitle>GLOBAL</ColumnTitle>
+                <TitledGroup title="CONNECTION">
+                    <ColumnsLayout
+                        stretchH
+                        extraClasses="sd1-patch-params__ops-enabled-row"
+                    >
+                        <Button
+                            highlighted={!is4Op}
+                            onClick={_handleSet2OpClicked}
                         >
-                            <Button
-                                highlighted={!is4Op}
-                                onClick={this._handleSet2OpClicked}
-                            >
-                                2-OP
-                            </Button>
-                            <Button
-                                highlighted={is4Op}
-                                onClick={this._handleSet4OpClicked}
-                            >
-                                4-OP
-                            </Button>
-                        </ColumnsLayout>
-                        {this.getConnectionButtons(is4Op)}
-                    </TitledGroup>
-                    <ColumnsLayout stretchH>
-                        <TitledGroup title="OCTAVE">
-                            <ParamKnob path={`${Modes.SD1}.bo`} size={60} />
-                        </TitledGroup>
-                        <TitledGroup title="LFO FREQ">
-                            <ParamKnob path={`${Modes.SD1}.lfo`} size={60} />
-                        </TitledGroup>
+                            2-OP
+                        </Button>
+                        <Button
+                            highlighted={is4Op}
+                            onClick={_handleSet4OpClicked}
+                        >
+                            4-OP
+                        </Button>
                     </ColumnsLayout>
-                </div>
-            </UIGroupEnabledContext.Provider>
-        );
-    };
+                    {getConnectionButtons(is4Op)}
+                </TitledGroup>
+                <ColumnsLayout stretchH>
+                    <TitledGroup title="OCTAVE">
+                        <ParamKnob path={`${Modes.SD1}.bo`} size={60} />
+                    </TitledGroup>
+                    <TitledGroup title="LFO FREQ">
+                        <ParamKnob path={`${Modes.SD1}.lfo`} size={60} />
+                    </TitledGroup>
+                </ColumnsLayout>
+            </div>
+        </UIGroupEnabledContext.Provider>
+    );
+};
 
-    _handleSet2OpClicked = () => this.props.onSet4Op(false);
-    _handleSet4OpClicked = () => this.props.onSet4Op(true);
-
-    getConnectionButtons = (is4Op) =>
-        is4Op ? (
-            <>
-                <ConnectionButton path={`${Modes.SD1}.alg`} set={2} />
-                <ConnectionButton path={`${Modes.SD1}.alg`} set={3} />
-                <ConnectionButton path={`${Modes.SD1}.alg`} set={4} />
-                <ConnectionButton path={`${Modes.SD1}.alg`} set={5} />
-                <ConnectionButton path={`${Modes.SD1}.alg`} set={6} />
-                <ConnectionButton path={`${Modes.SD1}.alg`} set={7} />
-            </>
-        ) : (
-            <>
-                <ConnectionButton path={`${Modes.SD1}.alg`} set={0} />
-                <ConnectionButton path={`${Modes.SD1}.alg`} set={1} />
-            </>
-        );
-}
+export default SD1GlobalParams;
