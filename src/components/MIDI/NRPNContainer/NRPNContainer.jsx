25d507bb5a1d2f32a2d5c9f89c0e59726e8a667c
Dan Fry <57286600+danielrfry@users.noreply.github.com>
2025-12-28 12:05:20 +0000

    Use Redux hooks in NRPNContainer.

--- src/components/MIDI/NRPNContainer/NRPNContainer.jsx
+++ src/components/MIDI/NRPNContainer/NRPNContainer.jsx
@@ -1,10 +1,15 @@
 import _ from 'lodash';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import NRPN from '../NRPN/NRPN';
+import { useCallback } from 'react';
 
-const mapStateToProps = (state, ownProps) => {
-    const { path } = ownProps;
-    return { value: _.get(state.patchEditor.patch, path) };
+const NRPNContainer = ({ path, ...props }) => {
+    const selectParamValue = useCallback(
+        (state) => _.get(state.patchEditor.patch, path),
+        [path]
+    );
+    const value = useSelector(selectParamValue);
+    return <NRPN value={value} {...props} />;
 };
 
-export default connect(mapStateToProps)(NRPN);
+export default NRPNContainer;
